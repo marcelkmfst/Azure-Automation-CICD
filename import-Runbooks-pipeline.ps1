618b5ab552449ed137131a9540204ab3a0f7de26
Marcel Keller <johndoeqexample.com>
2021-12-14 17:05:14 +0100

    added stages to pipeline
    Signed-off-by: Marcel Keller <johndoeqexample.com>

--- import-Runbooks-pipeline.ps1
+++ import-Runbooks-pipeline.ps1
@@ -7,6 +7,8 @@ $published # check how to add condition for published
 
 $type # find an way to set the target type based on the file extension
 
+# check if 
+# e.g. if $runbook = *.ps1 $filetype $Type = Powershell
 
 
 $runbooks=(Get-ChildItem -Path .\Runbooks\)
